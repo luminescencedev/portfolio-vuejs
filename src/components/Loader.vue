d80049f823a562acfb7ea694375d60c71e00a013
potichatmignon <arthur.garnier1090@gmail.com>
2025-02-10 15:24:22 +0100

    add timer in Loader.vue

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,31 +1,42 @@
 <script lang="ts" setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
+import { gsap } from 'gsap';
+
+const count = ref(0);
 
 onMounted(() => {
-    // const loader = document.querySelector('.loader') as HTMLElement;
-    
 
-    setTimeout(() => {
-        const loaderContainer = document.querySelector('.loader-container') as HTMLElement;
-        loaderContainer.classList.add('fade-out');
-        loaderContainer.addEventListener('animationend', () => {
-            loaderContainer.style.display = 'none';
-        });
-    }, 3000);
+    gsap.to(count, {
+        value: 100,
+        duration: 3,
+        ease: 'power1.inOut',
+        onUpdate: () => {
+            count.value = Math.round(count.value);
+        },
+        onComplete: () => {
+            const loaderContainer = document.querySelector('.loader-container') as HTMLElement;
+            loaderContainer.classList.add('fade-out');
+            loaderContainer.addEventListener('animationend', () => {
+                loaderContainer.style.display = 'none';
+            });
+        }
+    });
 });
 </script>
 
 <template>
     <div class="loader-container fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black z-50">
-        <div class="loader">
-            
+        <div class="loader h-full w-full">
+            <div class="flex items-center justify-center h-full">
+                <h1 class="text-9xl text-white font-[PPEditorialNew-Italic]">{{ count }} - 100</h1>
+            </div>
         </div>
     </div>
 </template>
 
 <style>
 .loader-container.fade-out {
-    animation: fadeOut 1s forwards;
+    animation: fadeOut 2s forwards;
 }
 
 @keyframes fadeOut {
